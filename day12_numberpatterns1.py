2e4582b031999419e455d6010ce5b1ed0b8eb65a
Sai Sreeja <saisreejaallampally@gmail.com>
2026-02-27 20:45:08 +0530

    Day 12 progress

---
+++ day12_numberpatterns1.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number of rows:"))
+for i in range(1,n+1):
+    for j in range(i):
+        print(j+1,end="")
+    print()
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
